de558fd0df66ffaa43610c9bac9847b747e0b4dc
titoValen <valentin.tito@davinci.edu.ar>
2026-09-16 15:22:13 -0300

    feat: Añadir configuración inicial del enrutador de Vue

---
+++ src/router/router.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from "vue-router";
+// TODO: Importar componentes de vista
+
+const routes = [
+  // TODO: Definir rutas
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
