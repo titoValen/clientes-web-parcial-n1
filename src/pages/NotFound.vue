df1cb72a2eae26796171b6fc81dabc0d5bf3b307
titoValen <valentin.tito@davinci.edu.ar>
2026-09-16 16:17:04 -0300

    feat: Crear componentes de página para Home, Login, Register y NotFound

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>404 - Página No Encontrada</h1>
+    <router-link to="/">Volver a la página de inicio</router-link>
+  </div>
+</template>
---
+++ src/pages/Register.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>Register</h1>
+  </div>
+</template>
