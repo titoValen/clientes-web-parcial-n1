2e2ba950e28dd6a2cb53b3e7da59f1f3883f5744
titoValen <valentin.tito@davinci.edu.ar>
2026-09-17 00:16:39 -0300

    feat: Agregar ruta para el componente Chat en el enrutador

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/Home.vue";
+import Chat from "../pages/Chat.vue";
 import Login from "../pages/Login.vue";
 import Register from "../pages/Register.vue";
 import NotFound from "../pages/NotFound.vue";
 
 const routes = [
   { path: "/", component: Home },
+  { path: "/chat", component: Chat },
   { path: "/login", component: Login },
   { path: "/register", component: Register },
   {
